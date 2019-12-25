bf66837ccaf01bdd4da65984869a5f44a5cd074f
Dmitry Dashkov <idimon66@gmail.com>
2019-12-25 18:45:22 +0200

    r

--- UsbSerialExampleApp/obj/Debug/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
+++ UsbSerialExampleApp/obj/Debug/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -5,7 +5,7 @@ public class XamarinAndroidEnvironmentVariables
 	// Variables are specified the in "name", "value" pairs
 	public static final String[] Variables = new String[] {
 		"MONO_LOG_LEVEL", "info",
-		"XAMARIN_BUILD_ID", "f1dd5690-f75b-4160-8af2-b32f0a3e6746",
+		"XAMARIN_BUILD_ID", "40f42bd4-7cfc-4b56-b33f-d5a57e393bad",
 		"XA_HTTP_CLIENT_HANDLER_TYPE", "System.Net.Http.HttpClientHandler, System.Net.Http",
 		"XA_TLS_PROVIDER", "btls",
 		"MONO_GC_PARAMS", "major=marksweep-conc",
